ec340529a249a17513c019503c7c4f641cde235d
Andrey <avtogol1998@gmail.com>
2019-11-12 12:55:06 +0200

    Added several test_cases

--- social_network_api/tests.py
+++ social_network_api/tests.py
@@ -5,7 +5,20 @@ from django.test import TestCase, Client
 from django.urls import reverse
 from rest_framework import status
 
+from social_network_api.models import Post
+
 client = Client()
+
+
+def get_access_token(user_data):
+    response = client.post(reverse('token_obtain_pair'),
+                           data=user_data,
+                           content_type='application/json')
+    access_token = response.data['access']
+    if response.status_code == status.HTTP_200_OK:
+        return access_token
+    else:
+        return None
 
 
 class SignInTest(TestCase):
@@ -61,6 +74,32 @@ class SignInTest(TestCase):
         self.assertAlmostEqual(response.data['email'], 'Email does not exist.')
 
 
+class LogInTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='alex', password='1111')
+        self.valid_user_data = {
+            'username': 'alex',
+            'password': '1111'
+        }
+        self.not_exist_user_data = {
+            'username': 'ivan',
+            'password': '1111'
+        }
+
+    def test_success_login(self):
+        response = client.post(reverse('token_obtain_pair'),
+                               data=self.valid_user_data,
+                               content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_login_not_existence_user(self):
+        response = client.post(reverse('token_obtain_pair'),
+                               data=self.not_exist_user_data,
+                               content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertAlmostEqual(response.data['detail'], 'No active account found with the given credentials')
+
+
 class CreatePostTest(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='alex', password='1111')
@@ -72,14 +111,60 @@ class CreatePostTest(TestCase):
             'title': 'Hobbit',
             'description': 'Movie',
         }
+        self.post_without_title_request = {
+            'description': 'Movie',
+        }
 
     def test_successful_create_post(self):
-        response = client.post(reverse('token_obtain_pair'),
-                               data=self.user_data,
-                               content_type='application/json')
-        access_token = response.data['access']
+        access_token = get_access_token(self.user_data)
         response = client.post(reverse('create_post'),
                                data=self.valid_post_request,
                                HTTP_AUTHORIZATION='Bearer ' + access_token,
                                content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_post_without_auth(self):
+        response = client.post(reverse('create_post'),
+                               data=self.valid_post_request,
+                               content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_create_post_without_title(self):
+        access_token = get_access_token(self.user_data)
+        response = client.post(reverse('create_post'),
+                               data=self.post_without_title_request,
+                               HTTP_AUTHORIZATION='Bearer ' + access_token,
+                               content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertAlmostEqual(response.data['title'][0], 'This field is required.')
+
+
+class LikePostTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='alex', password='1111')
+        self.post = Post.objects.create(title='Hobbit', description='Movie', created_by=self.user)
+        self.user_data = {
+            'username': 'alex',
+            'password': '1111'
+        }
+
+    def test_user_like_post(self):
+        access_token = get_access_token(self.user_data)
+        pk = self.post.id
+        response = client.put(reverse('like_post', kwargs={'pk': pk}),
+                              HTTP_AUTHORIZATION='Bearer ' + access_token,
+                              content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertAlmostEqual(response.data['success'],
+                               'Post {} like by {}'.format(self.post.title, self.user.username))
+
+    def test_user_unlike_post(self):
+        self.post.users_likes.add(self.user)  # user already like post
+        access_token = get_access_token(self.user_data)
+        pk = self.post.id
+        response = client.put(reverse('like_post', kwargs={'pk': pk}),
+                              HTTP_AUTHORIZATION='Bearer ' + access_token,
+                              content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertAlmostEqual(response.data['success'],
+                               'Post {} unlike by {}'.format(self.post.title, self.user.username))
--- social_network_api/urls.py
+++ social_network_api/urls.py
@@ -5,5 +5,5 @@ from social_network_api import views
 urlpatterns = [
     path('register/', views.RegistrationView.as_view(), name='register'),
     path('post/create/', views.PostCreateView.as_view(), name='create_post'),
-    path('post/like/<int:pk>/', views.PostLikesView.as_view()),
+    path('post/like/<int:pk>/', views.PostLikesView.as_view(), name='like_post'),
 ]
